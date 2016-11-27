75791f8719b5c44a600c1a1e85b21920321bbce4
rhindi <rhindi.youssef@courrier.uqam.ca>
2016-11-27 10:43:09 -0500

    remove the lance-all-Server.groovy

--- lance-all-Server.groovy
+++
@@ -1,4 +0,0 @@
-def lien = "Desktop/INF5153/TP2"
-def server8080 = ['/bin/bash', '-c', /osascript -e 'tell application "Terminal" to do script "cd $lien && groovy microservice_calcul_versement.groovy"'/].execute().text
-def server8081 = ['/bin/bash', '-c', /osascript -e 'tell application "Terminal" to do script "cd $lien && groovy microservice_calendrier_paiements.groovy"'/].execute().text
-def server8082 = ['/bin/bash', '-c', /osascript -e 'tell application "Terminal" to do script "cd $lien && groovy microservice_generateur_markdown.groovy "'/].execute().text
